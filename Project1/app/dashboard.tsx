c05b211310f48ac92c9e2ec176cfc39fe04e423b
Kaiser8960 <Dejitojim6@gmail.com>
2026-05-22 18:30:04 +0800

    Used router to connect homescreen to Dashboard

--- Project1/app/dashboard.tsx
+++ Project1/app/dashboard.tsx
@@ -1,3 +1,5 @@
+import { Button, Text, View } from 'react-native';
+
 export default function DashboardScreen() {
     return (
         <View style={{ flex: 1, justifyContent: 'center', alignItems: 'center' }}>
